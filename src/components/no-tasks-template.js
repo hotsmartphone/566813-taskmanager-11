3243615170e88f99a371b52a3a46c82dc81081bb
Hunter <v.kargalov@gmail.com>
2020-05-17 17:22:23 +0500

    Добавит закрытий карточки по Esс, отображение доски при отсутствии карточек

---
+++ src/components/no-tasks-template.js
@@ -0,0 +1,34 @@
+import {createElement} from "../utils.js";
+
+
+const createNoTasksTemplate = () => {
+  return (
+    `<p class="board__no-tasks">
+      Click «ADD NEW TASK» in menu to create your first task
+    </p>`
+  );
+};
+
+class NoTasks {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoTasksTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default NoTasks;
